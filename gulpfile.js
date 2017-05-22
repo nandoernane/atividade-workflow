70c2693c9db714a2e5a52433133d545c4923a2eb
Keven Jesus <falecom@keven.com.br>
2017-05-22 17:12:26 -0300

    faltou os plugins para compilar o scss e minificar o html

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require("gulp");
+var sass = require("gulp-sass");
+var htmlmin = require("gulp-htmlmin");
 
 gulp.task('move-style', function(){
 	return gulp.src('./source/scss/*.scss')
+	.pipe(sass({outputStyle: 'compressed'}))
 	.pipe(gulp.dest('./dist/css/'))
 });
 
@@ -9,6 +12,7 @@ gulp.task('move-style', function(){
 
 gulp.task('move-index', function(){
 	return gulp.src('./source/index.html')
+	.pipe(htmlmin({collapseWhitespace: true}))
 	.pipe(gulp.dest('./dist/'))
 });
 
